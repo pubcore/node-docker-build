0d639006c9048262f2faf088646c0b6cd2d78567
Jochen Probst <jpr@dds-mbpro-150.dmc.local>
2019-02-24 23:25:48 +0100

    add arguments for build funcs

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 'use strict'
 const {spawn} = require('child_process')
 
-const call = script => spawn(
-	`kill "$(pgrep -f 'node ${script}')" ; node  ${script} >  ../${script}.log 2>&1`,
+const call = (script, pkgName, domain) => spawn(
+	`kill "$(pgrep -f 'node ${script} ${pkgName} ${domain}')" ; node ${script} ${pkgName} ${domain} >  ../${script}.log 2>&1`,
 	{cwd:__dirname, detach:true, stdio:'ignore', shell:true}
 ).unref()
 
 module.exports = {
-	build: () => call('build'),
-	deploy: () => call('deploy'),
-	buildDeploy: () => call('build-deploy')
+	build: (pkgName, domain) => call('build', pkgName, domain),
+	deploy: (pkgName, domain) => call('deploy', pkgName, domain),
+	buildDeploy: (pkgName, domain) => call('build-deploy', pkgName, domain)
 }
