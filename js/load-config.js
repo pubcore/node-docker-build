643c8f8165b001fa4c704e07e22a3fc8e0d6a2bf
Jochen Probst <jpr@dds-mbpro-150.dmc.local>
2019-02-25 18:04:55 +0100

    fix allowed chars for args

--- js/load-config.js
+++ js/load-config.js
@@ -1,13 +1,13 @@
 'use strict'
 
 module.exports = (domainModule, domain) => {
-	if(domainModule.match(/[^a-zA-Z0-9@/\\.-]/)){
+	if(domainModule.match(/[^a-zA-Z0-9_.@/\\-]/)){
 		throw TypeError('illegal characters found in domain-module')
 	}
 	if(domain && domain.match(/[^a-z0-9_.-]/)){
 		throw TypeError('illegal characters found in domain')
 	}
-	
+
 	var config = require(domainModule)
 	//TODO create schema and validate
 
