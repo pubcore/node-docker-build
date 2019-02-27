76c621f80ae3a0a625589077615004accfa23d10
Jochen Probst <jpr@dds-mbpro-150.intra.dmc.de>
2019-02-27 14:07:58 +0100

    catch/log error, if an update of a  master package failes

--- js/lib/gitUpdatePackage.js
+++ js/lib/gitUpdatePackage.js
@@ -8,16 +8,19 @@ const {promisify} = require('util'),
 module.exports = ({dir, uri}) => new Promise((res, rej) => {
 	var scopeDir = path.resolve(dir, '..')
 	spawn('git', ['pull'], {cwd:dir})
-		.on('error', err => err && err.code === 'ENOENT'
-		&& mkdir(scopeDir, {recursive:true}).then(
-			() => spawn('git', ['clone', uri], {cwd:scopeDir, stdio:'inherit'})
-				.on('error', err => rej(console.log(err)))
-				.on('exit', code => (code === 0 ? res('hasChanged') : rej())),
-			err => rej(console.log(err))
-		) || rej(err))
+		.on('error', err =>
+			err && err.code === 'ENOENT' && mkdir(scopeDir, {recursive:true}).then(
+				() => spawn('git', ['clone', uri], {cwd:scopeDir, stdio:'inherit'})
+					.on('error', err => rej(console.log(err)))
+					.on('exit', code => (code === 0 ? res('hasChanged') : rej())),
+				err => rej(console.log(err))
+			) || rej(err))
 		.stdout.on('data', data =>
 			data.toString().match(/up to date/) ?
 				res(false)
-				: res('hasChanged')
+				: (data.toString().match(/Fast-forward/g) ?
+					res('hasChanged')
+					: rej(console.log(`ERROR git pull failed for ${dir}`))
+				)
 		)
 })
