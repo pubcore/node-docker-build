56753b74f56667318a1b8cacbae6f8c14a7d4b5e
Jochen Probst <jpr@dds-mbpro-150.dmc.local>
2019-03-16 00:01:04 +0100

    absolute path to host home required

--- js/lib/dockerBuild.js
+++ js/lib/dockerBuild.js
@@ -1,17 +1,18 @@
 'use strict'
 const path = require('path'),
-	{DOCKER_HOST, HOME} =  process.env,
-	compose = (command, path) => DOCKER_HOST ?
-		`docker run --rm -v /var/run/:/var/run -v ${HOME}:${HOME} -v ${HOME}:/root -v domains:/wd -w /wd/${path} docker/compose:1.23.2 ${command}`
-		: `docker-compose ${command}`,
+	{DOCKER_HOST} =  process.env,
 	cp = require('child_process')
 
 module.exports = config => new Promise((res, rej) => {
 	var {scope, name} = config.repository,
+		{home} = config.target,
 		subPath = `${scope}/${name}/${config.domain}`,
 		dir = path.resolve(config.baseDir, subPath),
-		command = `${compose('build', subPath)} && ${compose('push', subPath)}`
-	
+		compose = cmd => DOCKER_HOST ?
+			`docker run --rm -v /var/run/:/var/run -v ${home}:${home} -v ${home}:/root -v domains:/wd -w /wd/${subPath} docker/compose:1.23.2 ${cmd}`
+			: `docker-compose ${cmd}`,
+		command = `${compose('build')} && ${compose('push')}`
+
 	console.log(command)
 	cp.spawn( command, {cwd:dir, stdio:'inherit', shell:true} )
 		.on('exit', code => code === 0 ? res() : rej())
