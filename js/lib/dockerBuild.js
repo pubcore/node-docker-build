08e102691caa67a1c95a4ad9d615630283246f03
Jochen Probst <jpr@dds-mbpro-150.dmc.local>
2019-07-05 16:33:13 +0200

    add optional "buildCache" flag

--- js/lib/dockerBuild.js
+++ js/lib/dockerBuild.js
@@ -5,16 +5,19 @@ const path = require('path'),
 	buildArg = require('./buildArgs')
 
 module.exports = config => new Promise((res, rej) => {
-	var {repository, domain, target, buildArgs, push} = config,
+	var {repository, domain, target, buildArgs, push, buildCache} = config,
 		{scope, name, domainDir} = repository,
 		{home} = target,
 		subPath = `${scope}/${name}/${domainDir}/${domain}`,
 		dir = path.resolve(config.baseDir, subPath),
-		compose = cmd => DOCKER_HOST ?
-			`docker run --rm -v /var/run/:/var/run -v ${home}:${home} -v ${home}:/root -v domains:/wd -w /wd/${subPath} docker/compose:1.23.2 ${cmd}`
-			: `docker-compose ${cmd}`,
+		compose = (cmd, args='') => (
+			DOCKER_HOST ?
+				`docker run --rm -v /var/run/:/var/run -v ${home}:${home} -v ${home}:/root -v domains:/wd -w /wd/${subPath} docker/compose:1.23.2`
+				: 'docker-compose'
+		) + ` ${args} ${cmd} ${(cmd==='build' && buildArgs) ? buildArg(buildArgs):''}`,
+		buildCacheCommand = buildCache ? `${compose('build', '-f docker-compose-build-cache.yml')} && ` : '',
 		pushCommand = push ? ` && ${compose('push')} --ignore-push-failures ` : '',
-		command = `${compose('build')}${buildArgs ? buildArg(buildArgs):''}${pushCommand}`
+		command = `${buildCacheCommand}${compose('build')}${pushCommand}`
 
 	cp.spawn( command, {cwd:dir, stdio:'inherit', shell:true} )
 		.on('exit', code => code === 0 ? res() : rej())
