559ea6046889035949a3254283c1df4d799e9683
jacklrs <github@rushsoft.de>
2019-09-30 20:44:46 +0200

    correct working-dir for server build of js-master

--- js/lib/updateMasterPackages.js
+++ js/lib/updateMasterPackages.js
@@ -1,19 +1,22 @@
 'use strict'
 const updateMasterPackage = require('./updateMasterPackage'),
-	{resolve} = require('path'),
+	{resolve, basename, join} = require('path'),
 	throat = require('throat')(4),
 	dockerCompose = require('./dockerCompose'),
 	{promisify} = require('util'),
 	fs = require('fs'),
 	copyFile = promisify(fs.copyFile),
 	spawnCommand = require('./spawnCommand'),
-	buildArgs = require('./buildArgs')
+	buildArgs = require('./buildArgs'),
+	wdName = 'working-dir'
 
 //update master branch of all own packages (masterPackages)
 module.exports = async ({
 	masterPackages, workingDir, target, baseImage, sshHosts
 }) => {
-	var wd = resolve(workingDir, '..', '..', 'working-dir')
+
+	var packageDir = resolve(workingDir, '..', '..'),
+		wd = resolve(packageDir, wdName)
 	await Promise.all(Object.keys(masterPackages).reduce((acc, scope) => {
 		masterPackages[scope].packages.forEach(val => {
 			var {name, uri} = typeof val === 'string' ? {name:val} : val
@@ -30,9 +33,10 @@ module.exports = async ({
 			resolve(wd, file)
 		))
 	)
-	var args = {BASE_IMAGE:baseImage, SSH_HOSTS:sshHosts}
+	var args = {BASE_IMAGE:baseImage, SSH_HOSTS:sshHosts},
+		subPath = join(basename(packageDir), wdName)
 	await spawnCommand(
-		`${dockerCompose((target||{}).home, '_master-packages')} build ${buildArgs(args)}`,
+		`${dockerCompose((target||{}).home, subPath)} build ${buildArgs(args)}`,
 		wd
 	)
 }
