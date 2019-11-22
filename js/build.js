58db1925d5d989faba32b4b98873a2faf7287982
jacklrs <github@rushsoft.de>
2019-11-22 19:39:10 +0100

    kill child build processes on manual kill, too

--- js/build.js
+++ js/build.js
@@ -6,20 +6,23 @@ const build = require('./lib/build'),
 console.time('build')
 console.log('BEGIN')
 var [,,source, domain] = process.argv,
-	childProcesses = []
+	childProcesses = [],
+	killChilds = () => (childProcesses||[]).forEach(cp => {
+		try {
+			process.kill(-cp.pid)
+		} catch (e) {console.log(e.message)}
+	})
+
+process.on('error', killChilds)
+process.on('exit', killChilds)
+process.on('SIGINT', killChilds)
+
 loadConfig(source, domain).then(config => {
 	if(!config){
 		process.exit()
 	}
 	build(config, childProcesses).then(
 		() => console.timeEnd('build'),
-		err => {
-			console.log(`ERROR occured ${err}`)
-			;(childProcesses||[]).forEach(cp => {
-				try {
-					process.kill(-cp.pid)
-				} catch (e) {console.log(e.message)}
-			})
-		}
+		err => console.log('ERROR occured', err||'')
 	)
 })
