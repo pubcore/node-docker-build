3b7f6c66a3a0f098571f4522ac76427193620b18
jacklrs <github@rushsoft.de>
2020-09-22 19:07:09 +0200

    refactor: remove tests for legacy func

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,7 +1,5 @@
 const {expect} = require('chai'),
-	{build, deploy, buildDeploy, singletonExec} = require('../js/index'),
-	{execSync} = require('child_process'),
-	script = 'testScript',
+	{build, deploy, buildDeploy} = require('../js/index'),
 	script2 = 'test2Script'
 
 describe('build and deployment automation', () => {
@@ -12,13 +10,6 @@ describe('build and deployment automation', () => {
 		build({cwd:__dirname, script:script2, logPath:'./'})
 		deploy({cwd:__dirname, script:script2, logPath:'./'})
 		buildDeploy({cwd:__dirname, script:script2, logPath:'./'})
-	})
-	it('ensures only one process is running for given script', () => {
-		singletonExec({cwd:__dirname, script, logPath:'./'})
-		singletonExec({cwd:__dirname, script, logPath:'./'})
-		return new Promise(res => setTimeout(() => {
-			res(execSync('pgrep -f "^[^ ]*node .+testScript.+"').toString().trim())
-		}, 25)).then(res => expect(res).to.match(/^[0-9]+$/g))
 	})
 	it('supports "detach" boolean to enable synchronous build', () =>
 		build({cwd:__dirname, script:script2, logPath:'./', detach:false})
